f46a7ccbe508c1b9c0a37a2c1fd668f22e9b6e60
Shalom Dahan <75687634+VercetT1@users.noreply.github.com>
2021-05-15 20:17:00 +0300

    WTFFF

--- clientCEMS/src/application/ClientMain.java
+++ clientCEMS/src/application/ClientMain.java
@@ -21,7 +21,7 @@ public class ClientMain extends Application {
 	String hello = "hi";
 	private final int DEFAULT_PORT=5555;
 	ClientCEMS client;
-	String hi="hiConfilict";
+	String hi="WTFFFFFConfilict";
 	GUIControl guiControl=GUIControl.getInstance();
 	@Override
 	public void start(Stage primaryStage) {
