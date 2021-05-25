55095196e6aba3c8f736422782e01cc443084cd9
aviozkfir <Aviozkfiron@gmail.com>
2021-05-26 01:58:50 +0300

    Principal report

---
+++ clientCEMS/src/gui/PrincipalReportCourseControl.java
@@ -0,0 +1,20 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PrincipalReportCourseControl extends PrincipalReportController {
+    @FXML
+    private Button Back;
+
+    @FXML
+    void BackPressed(ActionEvent event) throws IOException {
+    	PrincipalExamBankSubjectsController a = (PrincipalExamBankSubjectsController) guiControl.loadStage(ClientsConstants.Screens.PRINCIPAL_EXAM_BANK_PAGE.path);
+		a.setPrincipalSubject();
+    }
+
+
+}
