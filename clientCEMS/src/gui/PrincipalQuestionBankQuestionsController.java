80ca2af46b12e6fd3a1cbd5000958427c55ed35c
onavioz <aviozavioz@gmail.com>
2021-06-07 00:52:01 +0300

    Principal- added search for question in question bank :)

--- clientCEMS/src/gui/PrincipalQuestionBankQuestionsController.java
+++ clientCEMS/src/gui/PrincipalQuestionBankQuestionsController.java
@@ -20,76 +20,86 @@ import message.ServerMessageTypes;
 public class PrincipalQuestionBankQuestionsController extends PrincipalMainPageController {
 	private ArrayList<Question> allQuestions;
 
-    @FXML
-    private AnchorPane myRoot;
-    
-    @FXML
-    private Text subjectName;
+	@FXML
+	private AnchorPane myRoot;
 
-    @FXML
-    private Text courseName;
+	@FXML
+	private Text subjectName;
 
-    @FXML
-    private TextField searchBar;
+	@FXML
+	private Text courseName;
 
-    @FXML
-    private Button btnSearch;
+	@FXML
+	private TextField searchBar;
 
-    @FXML
-    private Text numberOfQuestions;
+	@FXML
+	private Button btnSearch;
 
-    @FXML
-    private VBox vTable;
+	@FXML
+	private Text numberOfQuestions;
 
-    @FXML
-    private Button Back;
+	@FXML
+	private VBox vTable;
 
-    
-    private Course course;
+	@FXML
+	private Button Back;
 
-    @FXML
-    void BackPressed(ActionEvent event) throws IOException {
-    	PrincipalQuestionBankCoursesController contr =(PrincipalQuestionBankCoursesController) GUIControl.instance.loadStage
-    			(gui.ClientsConstants.Screens.PRINCIPAl_QUESTION_BANK_COURSES.path);
+	private Course course;
+
+	@FXML
+	void BackPressed(ActionEvent event) throws IOException {
+		PrincipalQuestionBankCoursesController contr = (PrincipalQuestionBankCoursesController) GUIControl.instance
+				.loadStage(gui.ClientsConstants.Screens.PRINCIPAl_QUESTION_BANK_COURSES.path);
 		contr.setPrincipalCourse(course.getSubject());
 		contr.setRequestCounter();
-    }
+	}
 
-    @FXML
-    void btnSearchPressed(ActionEvent event) {
+	@FXML
+	void btnSearchPressed(ActionEvent event) throws IOException {
+		PrincipalQuestionTableRowController controller;
+		String questionID = searchBar.getText();
+		boolean exist = false;
+		vTable.getChildren().clear(); 
+		for (Question q : allQuestions) {
+			if (q.getId().equals(questionID)) {
+				exist = true;
+				AddTableRow(q);
+				break;
+			}
+		}
+		if (!exist)
+			GUIControl.popUpError("There is no such a question in the list.");
+	}
 
-    }
-    
-    public void setPrincipalCourse(Course course) throws IOException {
-    	
-    	this.course=course;
-    	
-    	courseName.setText(course.getName());
-    	subjectName.setText(course.getSubject().getName());
-    	
-    	ClientMessage m1 = new ClientMessage(ClientMessageType.GET_QUESTION_BY_COURSE, course);
-    	guiControl.sendToServer(m1);
-    	
-    	allQuestions = (ArrayList<Question>) guiControl.getServerMsg().getMessage();
-    	
-    	numberOfQuestions.setText(""+allQuestions.size());
-    	
-    	for(Question q : allQuestions)
-    	{
-    		PrincipalQuestionTableRowController controller;
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/PrincipalQuestionTableRow.fxml"));
-			if (guiControl.getServerMsg().getType() == ServerMessageTypes.QUESTION_BY_COURSE_NOT_RECIVED) {
-				GUIControl.popUpError("Error-getting Question list for principal.");
-			} 
-			AnchorPane root = fxmlLoader.load();
-			controller = (PrincipalQuestionTableRowController) fxmlLoader.getController();
-			controller.setQuestion(q);
-			controller.setCourse(course);
-			vTable.getChildren().add(root);
+	@SuppressWarnings("unchecked")
+	public void setPrincipalCourse(Course course) throws IOException {
 
-			
-    	}
-    	
-    }
+		this.course = course;
+
+		courseName.setText(course.getName());
+		subjectName.setText(course.getSubject().getName());
+
+		ClientMessage m1 = new ClientMessage(ClientMessageType.GET_QUESTION_BY_COURSE, course);
+		guiControl.sendToServer(m1);
+		if (guiControl.getServerMsg().getType() == ServerMessageTypes.QUESTION_BY_COURSE_NOT_RECIVED) {
+			GUIControl.popUpError("Error-getting Question list for principal.");
+		}
+		allQuestions = (ArrayList<Question>) guiControl.getServerMsg().getMessage();
+		numberOfQuestions.setText("" + allQuestions.size());
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/PrincipalQuestionTableRow.fxml"));
+		for (Question q : allQuestions) {
+			AddTableRow(q);
+		}
+	}
+	
+	public void AddTableRow(Question q) throws IOException {
+		PrincipalQuestionTableRowController controller;
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/PrincipalQuestionTableRow.fxml"));
+		AnchorPane root = fxmlLoader.load();
+		controller = (PrincipalQuestionTableRowController) fxmlLoader.getController();
+		controller.setQuestion(q);
+		controller.setCourse(course);
+		vTable.getChildren().add(root);
+	}
 
 }
